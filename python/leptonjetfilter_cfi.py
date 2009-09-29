9f3aba59f4d84e4a084c0dccb5133388133adffc
ferencek <>
2009-09-29 17:44:30 +0000

    version for the October exercise

--- python/leptonjetfilter_cfi.py
+++ python/leptonjetfilter_cfi.py
@@ -1,27 +1,35 @@
 import FWCore.ParameterSet.Config as cms
 
 LJFilter = cms.EDFilter('LeptonJetFilter',
-                        # Specify number of muons
-                        muonsMin = cms.int32(1),
-                        muonsMax = cms.int32(-1),
-                        muPT     = cms.double(20.),
+    # Specify input collections
+    muLabel = cms.untracked.InputTag("cleanLayer1Muons"),
+    elecLabel = cms.untracked.InputTag("cleanLayer1Electrons"),
+    jetLabel = cms.untracked.InputTag("cleanLayer1Jets"),
 
-                        # Specify number of electrons
-                        electronsMin = cms.int32(1),
-                        electronsMax = cms.int32(-1),
-                        elecPT       = cms.double(20.),
+    # Specifiy number of jets
+    jetsMin = cms.int32(-1),
+    jetsMax = cms.int32(-1),
+    jetPT   = cms.double(30.),
+    jetEta  = cms.double(100.),
 
-                        # Specifiy number of jets
-                        jetsMin = cms.int32(-1),
-                        jetsMax = cms.int32(-1),
-                        jetPT   = cms.double(30.),
+    # Specify number of muons
+    muonsMin = cms.int32(1),
+    muonsMax = cms.int32(-1),
+    muPT     = cms.double(20.),
+    muEta    = cms.double(100.),
+    useMuID  = cms.bool(False),
+    muID     = cms.string("GlobalMuonPromptTight"),
 
-                        debug   = cms.untracked.bool(False),
-                        # If true, filter is true if either lepton condition met
-                        counteitherleptontype = cms.untracked.bool(True),
+    # Specify number of electrons
+    electronsMin = cms.int32(1),
+    electronsMax = cms.int32(-1),
+    elecPT       = cms.double(20.),
+    elecEta      = cms.double(100.),
+    useElecID    = cms.bool(False),
+    elecID       = cms.string("eidTight"),
 
-                        muLabel = cms.untracked.InputTag("cleanLayer1Muons"),
-                        elecLabel = cms.untracked.InputTag("cleanLayer1Electrons"),
-                        jetLabel = cms.untracked.InputTag("cleanLayer1Jets"),
+    debug   = cms.untracked.bool(False),
+    # If true, filter is true if either lepton condition met
+    counteitherleptontype = cms.bool(True)
 )
 
